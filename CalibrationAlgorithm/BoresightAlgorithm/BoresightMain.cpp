4c88eda0cfa3b1ca55395dede96dc0d304eae754
unknown <esther.cho@ucalgary.ca>
2019-02-23 20:43:24 -0700

    Added bundle adjustment code and files

--- CalibrationAlgorithm/BoresightAlgorithm/BoresightMain.cpp
+++ CalibrationAlgorithm/BoresightAlgorithm/BoresightMain.cpp
@@ -1,4 +1,5 @@
 #include "CalibrationFuntions.h"
+#include "BAFunctions.h"
 
 
 int main() {
@@ -167,7 +168,108 @@ int main() {
 	clog << "\n---------------------------Points---------------------\n";
 	print_vector(point_details);
 
-	// TODO: BUNDLE ADJUSTMENT
+	
+	
+	// BUNDLE ADJUSTMENT
+	MatrixXd planeparams;
+	MatrixXd scanparams;
+	MatrixXd pointparams;	
+	
+	//Convert vector of vectors to MatrixXd for plane, scene, and lidar point parameters
+	vec2mat(plane_details, planeparams, 4);
+	vec2mat(scene_details, scanparams, 6);
+	vec2mat(point_details, pointparams, 3);
+	
+	//Get number of planes, scans, lidar points
+	int numPlanes = planeparams.rows();
+	int numScans = scanparams.rows();
+	int numLidPts = pointparams.rows();
+	
+	//total number of unknowns
+	int u = 6 + 4*numPlanes + 6*numScans; 	
+	
+	//Initial boresight parameters set to zero
+	MatrixXd bs_params = MatrixXd::Zero(6,1);
+		
+	//Initialize matrices for LS
+	MatrixXd A(numScans*6+numLidPts, u);
+	MatrixXd A_full(numScans*6+numPlanes+numLidPts, u);
+	MatrixXd N(0,0);	
+	MatrixXd w(numScans*6+numLidPts, 1);
+	MatrixXd w_full(numScans*6+numPlanes+numLidPts, 1); 
+	MatrixXd U(0,0);	
+	MatrixXd H(numPlanes, u);
+	MatrixXd V(numPlanes ,1);	
+	MatrixXd B(u+numPlanes, u+numPlanes);
+	MatrixXd C(u+numPlanes,1);
+	MatrixXd Y(u+numPlanes,1);
+	
+	//Set Clo using GNSS_INS_data - TO DO **************************************
+	MatrixXd Clo = MatrixXd::Identity(numScans * 6 + numPlanes + numLidPts, numScans * 6 + numPlanes + numLidPts);
+		
+	//Compute P using Clo. Use apriorivf=1
+	MatrixXd P = Clo.inverse();
+	
+	double mean_Y = 1;
+	
+	while(mean_Y > 0.000001)
+	{
+		computeAandw(A_full, A, H, w_full, w, V, u, numPlanes, numScans, numLidPts, bs_params, planeparams, scanparams, pointparams, GNSS_INS_data);
+		//cout << "A=" << A_full << "\n\n";
+		//cout << "w=" << w << "\n\n";
+
+		N = A_full.transpose() * P * A_full; //Not sure if N is based on the full A, P with plane eqn derivatives or not???******************************
+		/* cout << "A^T=" << A_full.transpose() << "\n\n";
+		cout << "P" << P << "\n\n";
+		cout << "N=" << N << "\n\n"; */
+
+		U = -1 * A_full.transpose() * P * w_full; //Not sure if A, P, w here are based on the full A with plane eqn derivatives or not???******************************
+		//cout << "U=" << U << "\n\n";
+
+		B.block(0, 0, u, u) = N;
+		B.block(u, 0, numPlanes, u) = H;
+		B.block(0, u, u, numPlanes) = H.transpose();
+		B.block(u, u, numPlanes, numPlanes) = MatrixXd::Constant(numPlanes, numPlanes, 0);
+	
+		C.block(0,0,u,1) = U;
+		C.block(u,0,numPlanes,1) = V;
+		
+		Y = (B.transpose()*B).inverse() * B.transpose() * C;
+		//cout << "Y=" << Y << "\n\n";
+
+		//Calculate mean value of Y for termination condition
+		mean_Y = 0;
+		for(int i=0; i<(u+numPlanes); i++)
+		{
+			mean_Y = mean_Y + Y(i,0);
+		}
+		mean_Y = mean_Y/(u+numPlanes);
+		//cout << "mean Y=" << mean_Y << "\n\n";
+
+		//Update unknowns
+		
+		bs_params = bs_params + Y.block(0,0,6,1);
+
+		int Yindex = 6;
+		for (int i = 0; i < planeparams.rows(); i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				planeparams(i, j) = Y(Yindex,0);
+				Yindex++;
+			}
+		}
+
+		for (int i = 0; i < scanparams.rows(); i++)
+		{
+			for (int j = 0; j < 6; j++)
+			{
+				scanparams(i, j) = Y(Yindex, 0);
+				Yindex++;
+			}
+		}
+
+	}
 
 
 	// GEOREFERENCE
